249d98cc9c328e94815ee2d77c3501720a5c7630
huhujizhuanyi <30588962+huhujizhuanyi@users.noreply.github.com>
2020-08-03 11:40:36 +0800

    Update ReadWriteQueue.java

--- Concurrency/ReadWriteQueue.java
+++ Concurrency/ReadWriteQueue.java
@@ -13,15 +13,15 @@ public class ReadWriteQueue<E>{
 	private final Condition notFull = lock.newCondition();
 	private final Condition notEmpty = lock.newCondition();
 	
-    static class Node<E> {
-        E item;
-        Node<E> next;
-        Node(E x) {
-        	item = x;
-        }
-        Node(){
-        }
-    }
+	static class Node<E> {
+		E item;
+		Node<E> next;
+		Node(E x) {
+			item = x;
+		}
+		Node(){
+		}
+	}
 	
 	public ReadWriteQueue(int s){
 		size = s;
@@ -29,39 +29,39 @@ public class ReadWriteQueue<E>{
 	}
 	
 	public void push(E x) throws InterruptedException{
-	    lock.lock();
-	    try {
-	    	while(size == capacity) {
-	    		notFull.await();
-	    	}
-    		Node<E> tmp = new Node<E>(x);
-	    	Node<E> p = head;
-	    	while(p.next != null) {
-	    		p = p.next;
-	    	}
-	    	p.next = tmp;
-	    	size++;
-	    	notEmpty.signal();
-	    }finally {
-	        lock.unlock();
-	    }
+		lock.lock();
+		try {
+			while(size == capacity) {
+				notFull.await();
+			}
+			Node<E> tmp = new Node<E>(x);
+			Node<E> p = head;
+			while(p.next != null) {
+				p = p.next;
+			}
+			p.next = tmp;
+			size++;
+			notEmpty.signal();
+		}finally {
+			lock.unlock();
+		}
 	}
 	
 	public E pop() throws InterruptedException{
-	    lock.lock();
-	    try {
-	    	while(size == 0 || head == null) {
-	    		notEmpty.await();
-	    	}
-    		Node<E> p = head;
-	    	head = head.next;
-	    	p.next = null;
-	    	size--;
-	    	notFull.signal();
-	        return p.item;
-	    } finally {
-	        lock.unlock();
-	    }
+		lock.lock();
+		try {
+			while(size == 0 || head == null) {
+				notEmpty.await();
+			}
+			Node<E> p = head;
+			head = head.next;
+			p.next = null;
+			size--;
+			notFull.signal();
+			return p.item;
+		} finally {
+			lock.unlock();
+		}
 	}
 	
 	public int getSize() {
